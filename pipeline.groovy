83a65cd0af6d57d46ca1aaa156330a4d76e40420
Huy Phan <phan.nguyen.huy@zalando.fi>
2017-02-21 03:10:37 +0200

    #11 Testing

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,18 +2,37 @@
   node {
       stage("Build") {
           echo "Building"
+          notifyStarted()
       }
-
-      stage("Test") {
-          echo "Testing"
+      stage("Unit Test") {
+          echo "Unit testing"
       }
-
       branch(/^master$/){
+          stage("Pushing to stage") {
+              echo "Push"
+          }
+          stage("Trigger stage job") {
+              echo "Deploying"
+          }
+      }
+
+
+      branch(/^stage$/){
+          stage("Acceptance Test") {
+              echo "Testing"
+          }
           stage("Deploy") {
               echo "Deploying"
           }
       }
   }
+}
+
+def notifyStarted() {
+  // send to HipChat
+  hipchatSend (color: 'YELLOW', notify: true,
+      message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+    )
 }
 
 @NonCPS
