8d53d6cf26ca02bc374e3835e8120ccf34f1e246
Huy Phan <phan.nguyen.huy@zalando.fi>
2017-02-21 03:27:58 +0200

    #tele hehehe

--- pipeline.groovy
+++ pipeline.groovy
@@ -31,8 +31,19 @@
 def notifyStarted() {
   // send to HipChat
   hipchatSend (color: 'YELLOW', notify: true,
-      message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+      message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) cause (${env.BUILD_CAUSE}) id (${env.BUILD_ID}) commit (${env.GIT_COMMIT}) branch (${env.GIT_BRANCH}) committer ${git show -s --pretty=%an}"
     )
+}
+
+@NonCPS
+def summarizeBuild(b) {
+  b.changeSets.collect { cs ->
+    /kind=${cs.kind}; entries=/ + cs.collect { entry ->
+      /${entry.commitId} by ${entry.author.id} ~ ${entry.author.fullName} on ${new Date(entry.timestamp)}: ${entry.msg}:  /+ entry.affectedFiles.collect { file ->
+        /${file.editType.name} ${file.path}/
+      }.join('; ')
+    }.join(', ')
+  }.join(' & ')
 }
 
 @NonCPS
