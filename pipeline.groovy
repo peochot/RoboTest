286359f4a4350f950628a49d7effd4e3deb2f816
Huy Phan <phan.nguyen.huy@zalando.fi>
2017-02-21 12:01:12 +0200

    Test

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,10 +2,16 @@
   node {
       stage("Build") {
           echo "Building"
-          def build = currentBuild
-          currentBuild.result = "STARTED"
-          def color = "GREEN"
-          notify(build, color)
+          try {
+            def build = currentBuild
+            currentBuild.result = "STARTED"
+            def color = "GREEN"
+            notify(build, color)
+          } catch(e) {
+            currentBuild.result = "FAILED"
+            notify(build, color)
+            throw e;
+          }
       }
 
       stage("Unit Test") {
@@ -27,7 +33,7 @@
 
 def notify(build, color) {
   hipchatSend (color: color, notify: true, message: "Job: '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) branch (${env.GIT_BRANCH}) \n "
-                                                      + "Status : ${build.result}  ${summarizeBuild(build)}")
+                                                      + "Status : ${build.result}")
 }
 
 @NonCPS
